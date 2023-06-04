3c83885673376b5b05e3676e7bb0f3d96eef3b47
Manuel Teixeira <manuel.teixeira@tui.co.uk>
2023-06-04 11:32:51 +0100

    Initial Commit

---
+++ padel/padelApp.swift
@@ -0,0 +1,17 @@
+//
+//  padelApp.swift
+//  padel
+//
+//  Created by Manuel Teixeira on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct padelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
